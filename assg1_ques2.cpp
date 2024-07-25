fd87aafc2cb8ad51d9c3c8e3ea649ba6884a8d36
vimarshmishra <22103142@mail.jiit.ac.in>
2024-07-25 15:38:47 +0530

    Add files via upload
    ques 3 chqanges

--- assg1_ques2.cpp
+++ assg1_ques2.cpp
@@ -2,15 +2,9 @@
 using namespace std;
 int main()
 {
-
-    vector<int>arr;
-    for(int i=0;i<9;i++)
-    {
-
-        arr.push_back(rand()%100);
-    }
-    float x=0;
-    for(auto i:arr)
-        x+=i;
-    cout<<x/9;
+    string x;
+    cin>>x;
+    for(int i=0;i<x.size()/2;i++)
+        swap(x[i],x[x.size()-i-1]);
+    cout<<x;
 }
